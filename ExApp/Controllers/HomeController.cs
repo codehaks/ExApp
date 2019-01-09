40b1800b5dbfe5e2222a5eb5449473b801820d77
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-10 00:13:42 +0330

    Fix

--- ExApp/Controllers/HomeController.cs
+++ ExApp/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ExApp.Controller
+namespace ExApp.Controllers
 {
     public class HomeController : Controller
     {
