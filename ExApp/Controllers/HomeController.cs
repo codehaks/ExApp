5f94db0d18aeae3dee0da068e0d1eec4a5d24a69
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-10 20:38:25 +0330

    Scripting Added

--- ExApp/Controllers/HomeController.cs
+++ ExApp/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Reflection;
 
 namespace ExApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ScriptOptions ScriptOpts = ScriptOptions.Default.AddReferences(
+                    typeof(HomeController).GetTypeInfo().Assembly,
+                    typeof(HttpContext).GetTypeInfo().Assembly)
+            .AddImports("ScriptingInWebAppDemo", "Microsoft.AspNetCore.Http");
+
         public IActionResult Index()
         {
             return View();
